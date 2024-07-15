01d176e8c3aa582083191b7adda6fdef711e5743
Chelsea <chelsea@tripwiretech.com>
2024-07-15 21:59:15 +1200

    Tutorial

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  Keyboard,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [keyboardState, setKeyboardState] = useState("Keyboard closed");
+
+  useEffect(() => {
+    const keyboardShowingSub = Keyboard.addListener("keyboardDidShow", () => {
+      setKeyboardState("Keyboard open");
+    });
+
+    const keyboardHidingSub = Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboardState("Keyboard closed");
+    });
+
+    return () => {
+      keyboardHidingSub.remove();
+      keyboardShowingSub.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{keyboardState}</Text>
+      <TextInput style={styles.textInput} placeholder="Text goes here..." />
+      <Button title="Dismiss" onPress={Keyboard.dismiss} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    margin: 16,
+    padding: 5,
+    borderColor: "black",
+    borderWidth: 1,
+    alignSelf: "stretch",
   },
 });
